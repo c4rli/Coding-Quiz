ab6e67d6b7e3a23c96dbea175714b39a47f51647
xarli <117117311+c4rli@users.noreply.github.com>
2023-01-13 19:03:23 +0000

    Inital upload

---
+++ assets/js/questions.js
@@ -0,0 +1,30 @@
+// Quiz questions and answers
+var questions = [
+    {
+        question: "Question 1",
+        choices: ["ans1", "ans2", "ans3", "ans4"],
+        answer: 1
+    },
+    {
+        question: "Question 2",
+        choices: ["ans1", "ans2", "ans3", "ans4"],
+        answer: 3
+    },
+    {
+        question: "Question 3",
+        choices: ["ans1", "ans2", "ans3", "ans4"],
+        answer: 0
+    },
+    {
+        question: "Question 4",
+        choices: ["ans1", "ans2", "ans3", "ans4"],
+        answer: 0
+    },
+    {
+        question: "Question 5",
+        choices: ["ans1", "ans2", "ans3", "ans4"],
+        answer: 2
+    }
+];
+
+
