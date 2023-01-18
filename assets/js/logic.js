5b4038e9096a5cdebbdae01c3b74c1833d26224a
xarli <117117311+c4rli@users.noreply.github.com>
2023-01-18 23:25:57 +0000

    Code cleanup + comments

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,91 +1,78 @@
 // Global variables
-var currentQuestion = 0;
-var timer = 150;
-var score = 0;
-var countdownTime;
-var timerEl;
+var currentQuestion = 0; // keep track of current question 
+var timer = 150; // set initial timer value
+var score = 0; // keep track of player's score
+var countdownTime; // variable to hold countdown time
+var timerEl; // variable to hold setInterval() timer
 
 //DOM variables 
-
+//Views on index.html
 var startScreen = document.querySelector("#start-screen");
 var questionsScreen = document.querySelector("#questions");
 var endScreen = document.querySelector("#end-screen");
 
+// Question DOM elements
 var questionTitle = document.querySelector("#question-title");
 var questionOptions = document.querySelector("#choices");
 
+// End screen DOM elements
 var inputInitials = document.querySelector("#initials");
 var submitScore = document.querySelector("#submit");
 var time = document.querySelector("#time");
 
+// Load highscores on page load.
 loadHighscores();
-
-// var highscoresEl = document.querySelector("#highscores");
 time.textContent = timer;
 
+// Get highscores from localstorage to highscores var or set highscores to blank array if localstorage doesnt exist
 function loadHighscores() {
     if (localStorage.getItem("highscoresStorage") == '' || localStorage.getItem("highscoresStorage") === null) {
         highscores = [];
-    }else {
+    } else {
         highscores = JSON.parse(localStorage.getItem("highscoresStorage"));
     }
-    }
+}
 
+// playGame() runs when start quiz button is pressed. Changes the views by hiding relevant elements
+// Starts countdown and display question
 function playGame() {
-    startScreen.setAttribute("class","hide");
+    startScreen.setAttribute("class", "hide");
     questionsScreen.removeAttribute("class", "hide");
-    
+
     countdownTimer(timer);
-
-    // var interval = setInterval(function() {
-    //     if (timer > 0) {
-    //         time.textContent = timer;
-    //         timer--;
-    //     } else {
-    //         gameOver();
-    //         clearInterval(interval);
-    //     }
-    // }, 1000);
-
     displayQuestion();
 }
 
+// Clear questions from previous question and replace with unordered list
+// Get questions from questions.js and populate correct amount of buttons with text label.
 function displayQuestion() {
     questionOptions.innerHTML = ('');
     var options = document.createElement("ul");
-    
+
     questionTitle.textContent = questions[currentQuestion].question;
-    
-    questions[currentQuestion].choices.forEach( function(choice, i) {
+
+    questions[currentQuestion].choices.forEach(function (choice, i) {
         var option = document.createElement("button");
         option.textContent = choice;
-        options.appendChild(option); 
+        options.appendChild(option);
     })
     questionOptions.appendChild(options);
-}   
+}
 
-// function displayHighscores() {
-
-//     highscoresEl.innerHTML = ('');
-//     highscores.forEach( function(values, i) {
-//         var entry = document.createElement("ul");
-//         entry.textContent = values;
-//         highscoresEl.appendChild(entry); 
-//     });
-
-//     // window.location.href = 'highscores.html';
-// }
-
+// End game event triggered by reaching last question or time running out.
+// Changes view for player to enter initials
 function gameOver() {
     score = timer;
     clearInterval(timerEl);
     alert("game Over");
-    questionsScreen.setAttribute("class","hide");
+    
+    questionsScreen.setAttribute("class", "hide");
     endScreen.removeAttribute("class", "hide");
 }
 
+// Check if question completed is the last question
 function nextQuestion() {
-    if (currentQuestion == questions.length){
+    if (currentQuestion == questions.length) {
         gameOver();
     }
     else {
@@ -93,11 +80,10 @@ function nextQuestion() {
     }
 }
 
+// Timer that counts down every second, calls gameOver() if reaches 0
 function countdownTimer(seconds) {
-    // var count = seconds;
-    timerEl = setInterval(function() {
+    timerEl = setInterval(function () {
         if (timer > 0) {
-            // timer = count;
             time.textContent = timer;
             timer--;
         } else {
@@ -107,37 +93,33 @@ function countdownTimer(seconds) {
     }, 1000);
 }
 
-questionOptions.addEventListener("click", function(event) {
+// Event listener for answer buttons
+questionOptions.addEventListener("click", function (event) {
     var element = event.target;
-  
-    if (element.matches("button")) {
-      var state = element.textContent;
-      var answerIndex = questions[currentQuestion].answer;
 
-        if (state == questions[currentQuestion].choices[answerIndex])
-        {
+    if (element.matches("button")) {
+        var state = element.textContent;
+        var answerIndex = questions[currentQuestion].answer;
+
+        if (state == questions[currentQuestion].choices[answerIndex]) {
             alert("Correct");
         }
         else {
-            timer -=10;
+            timer -= 10;
             alert("Wrong");
         }
         currentQuestion++;
         nextQuestion();
     }
-  });
+});
 
-  submitScore.addEventListener("click", function(event) {
+// Event listener for highscore submission screen
+submitScore.addEventListener("click", function (event) {
     var playerScore = score;
     var playerInitials = inputInitials.value;
-    // var scoreArray = [];
-    // scoreArray.push(playerInitials);
-    // scoreArray.push(playerScore);
-    highscores.push([playerInitials,playerScore]);
 
+    highscores.push([playerInitials, playerScore]);
     localStorage.setItem("highscoresStorage", JSON.stringify(highscores));
 
-    // displayHighscores();
-    // alert();
     window.location.href = 'highscores.html';
-  });
+});
--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,31 +1,35 @@
 var highscoresEl = document.querySelector("#highscores");
 
+// Get highscores from localstorage to highscores var or set highscores to blank array if localstorage doesnt exist
 function loadHighscores() {
-if (localStorage.getItem("highscoresStorage") == '' || localStorage.getItem("highscoresStorage") === null) {
-    highscores = [];
-}else {
-    highscores = JSON.parse(localStorage.getItem("highscoresStorage"));
+    if (localStorage.getItem("highscoresStorage") == '' || localStorage.getItem("highscoresStorage") === null) {
+        highscores = [];
+    } else {
+        highscores = JSON.parse(localStorage.getItem("highscoresStorage"));
+    }
 }
-}
+
+// Populate ul with elements loaded from localstorage.
 function displayHighscores() {
 
     highscoresEl.innerHTML = ('');
-    if(!highscores == []){
-    highscores.forEach( function(values, i) {
-        var entry = document.createElement("li");
-        entry.textContent = (`${values[0]} - ${values[1]}`);
-        highscoresEl.appendChild(entry); 
-    });
-}
-    
+    if (!highscores == []) {
+        highscores.forEach(function (values, i) {
+            var entry = document.createElement("li");
+            entry.textContent = (`${values[0]} - ${values[1]}`);
+            highscoresEl.appendChild(entry);
+        });
+    }
 }
 
-function resetScores () {
+// Set the value of localstorage highscores to an empty array
+function resetScores() {
     var emptyArray = [];
     localStorage.setItem("highscoresStorage", emptyArray);
     alert("Scores Cleared!")
 }
 
+// Load or generate highscores and add them to page
 loadHighscores();
 displayHighscores();
 
